96bb3f2706da3f20b158aa0c33e1d66912815095
Zakaria AITOUMERIEM <zakariaaitoumeriem1889@gmail.com>
2021-02-26 00:36:25 +0100

    Button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button
+      :style="{ background: color }"
+      class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
